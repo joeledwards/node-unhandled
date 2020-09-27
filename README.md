daba8da6e1134ee3a662774363c9047a0f72628f
Joel Edwards <joeledwards@gmail.com>
2020-09-27 07:32:51 -0600

    breaking: added handling for SIGINT and SIGTERM

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # unhandled
 
-Adds handlers for the `uncaughtException` and `unhandledRejection` events. The default behavior is to log them to console then exit with code `1`.
+Adds handlers for the `uncaughtException`, `unhandledRejection`, `SIGINT`, and `SIGTERM events.
+The default behavior is to log the event to console (and the error, if applicable), then exit with code `1`.
 
 The logger, exit code, and optional handler(s) can be configured
 
 `unhandle(options)`
-- `options.handler`: `({ event, error }) => nil` | Generic handler function for unhandled events.
+- `options.handler`: `({ event, error? }) => nil` | Generic handler function for unhandled events.
 - `options.logger`: `{ error(...) => nil }` = `console` | The logger to which the unhandled event errors should be logged.
 - `options.exit`: `boolean` = `false` | Indicates whether unhandled events should trigger runtime exit.
 - `options.exception.exit`: `boolean` | Exit on uncaught exception (supercedes `options.exit`).
@@ -14,3 +15,9 @@ The logger, exit code, and optional handler(s) can be configured
 - `options.rejection.exit`: `boolean` | Exit on unhandled promise rejection (supercedes `options.exit`).
 - `options.rejection.handler`: `({ event, error }) => nil` | Custom, synchronous action to run on unhandled promise rejection (supercedes `options.handler`).
 - `options.rejection.ignore`: `boolean` = `false` | Do not handle rejections (permit default runtime behavior).
+- `options.sigint.exit`: `boolean` | Exit on SIGINT event (supercedes `options.exit`).
+- `options.sigint.handler`: `({ event }) => nil` | Custom, synchronous action to run on SIGINT signal (supercedes `options.handler`).
+- `options.sigint.ignore`: `boolean` = `false` | Do not handle SIGINT (permit default runtime behavior).
+- `options.sigterm.exit`: `boolean` | Exit on SIGTERM event (supercedes `options.exit`).
+- `options.sigterm.handler`: `({ event }) => nil` | Custom, synchronous action to run on SIGTERM signal (supercedes `options.handler`).
+- `options.sigterm.ignore`: `boolean` = `false` | Do not handle SIGTERM (permit default runtime behavior).
--- test/index.test.js
+++ test/index.test.js
@@ -19,6 +19,8 @@ async function runTest (config, action) {
   const context = {
     doReject: error => events.emit('unhandledRejection', error),
     doThrow: error => events.emit('uncaughtException', error),
+    doSigint: () => events.emit('SIGINT'),
+    doSigterm: () => events.emit('SIGTERM'),
     exitCalls: () => exitCalls,
     logs: () => logs
   }
@@ -42,13 +44,17 @@ async function runTest (config, action) {
 
 tap.test('default config just logs the error', async assert => {
   await runTest(c => {
-    c.doReject('async-fail')
-    c.doThrow('sync-fail')
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
 
     assert.equal(c.exitCalls().length, 0)
-    assert.equal(c.logs().length, 2)
-    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'async-fail'])
-    assert.same(c.logs()[1], ['Un-caught Exception :', 'sync-fail'])
+    assert.equal(c.logs().length, 4)
+    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'rejected'])
+    assert.same(c.logs()[1], ['Un-caught Exception :', 'excepted'])
+    assert.same(c.logs()[2], ['SIGINT Received'])
+    assert.same(c.logs()[3], ['SIGTERM Received'])
   })
 })
 
@@ -56,11 +62,15 @@ tap.test('custom handler can be passed in', async assert => {
   const metrics = meter()
   const handler = ({ event, error }) => metrics.add(`handler:${event}:${error}`)
   await runTest({ handler }, c => {
-    c.doReject('async-fail')
-    c.doThrow('sync-fail')
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
 
-    assert.equal(metrics.get('handler:unhandledRejection:async-fail'), 1)
-    assert.equal(metrics.get('handler:uncaughtException:sync-fail'), 1)
+    assert.equal(metrics.get('handler:unhandledRejection:rejected'), 1)
+    assert.equal(metrics.get('handler:uncaughtException:excepted'), 1)
+    assert.equal(metrics.get('handler:SIGINT:undefined'), 1)
+    assert.equal(metrics.get('handler:SIGTERM:undefined'), 1)
   })
 })
 
@@ -69,33 +79,75 @@ tap.test('event-specific handlers will override the global handler', async asser
   const globalHandler = ({ event, error }) => metrics.add(`globalHandler:${event}:${error}`)
   const exceptionHandler = ({ event, error }) => metrics.add(`exceptionHandler:${event}:${error}`)
   const rejectionHandler = ({ event, error }) => metrics.add(`rejectionHandler:${event}:${error}`)
+  const sigintHandler = ({ event, error }) => metrics.add(`sigintHandler:${event}:${error}`)
+  const sigtermHandler = ({ event, error }) => metrics.add(`sigtermHandler:${event}:${error}`)
 
   await runTest({ handler: globalHandler, exception: { handler: exceptionHandler } }, c => {
-    c.doReject('async-fail')
-    c.doThrow('sync-fail')
+    metrics.clear()
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
 
-    assert.equal(metrics.get('globalHandler:unhandledRejection:async-fail'), 1)
-    assert.equal(metrics.get('exceptionHandler:uncaughtException:sync-fail'), 1)
+    assert.equal(metrics.get('globalHandler:unhandledRejection:rejected'), 1)
+    assert.equal(metrics.get('globalHandler:SIGINT:undefined'), 1)
+    assert.equal(metrics.get('globalHandler:SIGTERM:undefined'), 1)
+    assert.equal(metrics.get('exceptionHandler:uncaughtException:excepted'), 1)
   })
 
   await runTest({ handler: globalHandler, rejection: { handler: rejectionHandler } }, c => {
-    c.doReject('async-fail')
-    c.doThrow('sync-fail')
+    metrics.clear()
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
 
-    assert.equal(metrics.get('globalHandler:uncaughtException:sync-fail'), 1)
-    assert.equal(metrics.get('rejectionHandler:unhandledRejection:async-fail'), 1)
+    assert.equal(metrics.get('globalHandler:uncaughtException:excepted'), 1)
+    assert.equal(metrics.get('globalHandler:SIGINT:undefined'), 1)
+    assert.equal(metrics.get('globalHandler:SIGTERM:undefined'), 1)
+    assert.equal(metrics.get('rejectionHandler:unhandledRejection:rejected'), 1)
+  })
+
+  await runTest({ handler: globalHandler, sigint: { handler: sigintHandler } }, c => {
+    metrics.clear()
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
+
+    assert.equal(metrics.get('globalHandler:uncaughtException:excepted'), 1)
+    assert.equal(metrics.get('globalHandler:SIGTERM:undefined'), 1)
+    assert.equal(metrics.get('globalHandler:unhandledRejection:rejected'), 1)
+    assert.equal(metrics.get('sigintHandler:SIGINT:undefined'), 1)
+  })
+
+  await runTest({ handler: globalHandler, sigterm: { handler: sigtermHandler } }, c => {
+    metrics.clear()
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
+
+    assert.equal(metrics.get('globalHandler:uncaughtException:excepted'), 1)
+    assert.equal(metrics.get('globalHandler:unhandledRejection:rejected'), 1)
+    assert.equal(metrics.get('globalHandler:SIGINT:undefined'), 1)
+    assert.equal(metrics.get('sigtermHandler:SIGTERM:undefined'), 1)
   })
 })
 
 tap.test('exits if exit is true', async assert => {
   await runTest({ exit: true }, c => {
-    c.doReject('async-fail')
-    c.doThrow('sync-fail')
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
 
-    assert.equal(c.exitCalls().length, 2)
-    assert.equal(c.logs().length, 2)
-    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'async-fail'])
-    assert.same(c.logs()[1], ['Un-caught Exception :', 'sync-fail'])
+    assert.equal(c.exitCalls().length, 4)
+    assert.equal(c.logs().length, 4)
+    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'rejected'])
+    assert.same(c.logs()[1], ['Un-caught Exception :', 'excepted'])
+    assert.same(c.logs()[2], ['SIGINT Received'])
+    assert.same(c.logs()[3], ['SIGTERM Received'])
   })
 })
 
@@ -103,107 +155,279 @@ tap.test('event-specific exit config supercedes global exit config', async asser
   const metrics = meter()
   const exitObserver = (code) => metrics.add('exit')
 
-  await runTest({ exit: true, exception: { exit: false }, rejection: { exit: false }, context: { exit: exitObserver } }, c => {
-    c.doReject('async-fail')
+  await runTest({
+    exit: true,
+    exception: { exit: false },
+    rejection: { exit: false },
+    sigint: { exit: false },
+    sigterm: { exit: false },
+    context: { exit: exitObserver }
+  }, c => {
+    c.doReject('rejected')
     assert.equal(metrics.get('exit'), 0)
 
-    c.doThrow('sync-fail')
+    c.doThrow('excepted')
+    assert.equal(metrics.get('exit'), 0)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 0)
+
+    c.doSigterm()
     assert.equal(metrics.get('exit'), 0)
   })
 
   metrics.clear()
 
-  await runTest({ exit: false, exception: { exit: true }, rejection: { exit: true }, context: { exit: exitObserver } }, c => {
-    c.doReject('async-fail')
+  await runTest({
+    exit: false,
+    exception: { exit: true },
+    rejection: { exit: true },
+    sigint: { exit: true },
+    sigterm: { exit: true },
+    context: { exit: exitObserver }
+  }, c => {
+    c.doReject('rejected')
     assert.equal(metrics.get('exit'), 1)
 
-    c.doThrow('sync-fail')
+    c.doThrow('excepted')
     assert.equal(metrics.get('exit'), 2)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 3)
+
+    c.doSigterm()
+    assert.equal(metrics.get('exit'), 4)
   })
 
   metrics.clear()
 
   await runTest({ exit: false, exception: { exit: true }, context: { exit: exitObserver } }, c => {
-    c.doReject('async-fail')
+    c.doReject('rejected')
     assert.equal(metrics.get('exit'), 0)
 
-    c.doThrow('sync-fail')
+    c.doThrow('excepted')
+    assert.equal(metrics.get('exit'), 1)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 1)
+
+    c.doSigterm()
     assert.equal(metrics.get('exit'), 1)
   })
 
   metrics.clear()
 
   await runTest({ exit: false, rejection: { exit: true }, context: { exit: exitObserver } }, c => {
-    c.doReject('async-fail')
+    c.doReject('rejected')
     assert.equal(metrics.get('exit'), 1)
 
-    c.doThrow('sync-fail')
+    c.doThrow('excepted')
+    assert.equal(metrics.get('exit'), 1)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 1)
+
+    c.doSigterm()
+    assert.equal(metrics.get('exit'), 1)
+  })
+
+  metrics.clear()
+
+  await runTest({ exit: false, sigint: { exit: true }, context: { exit: exitObserver } }, c => {
+    c.doReject('rejected')
+    assert.equal(metrics.get('exit'), 0)
+
+    c.doThrow('excepted')
+    assert.equal(metrics.get('exit'), 0)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 1)
+
+    c.doSigterm()
+    assert.equal(metrics.get('exit'), 1)
+  })
+
+  metrics.clear()
+
+  await runTest({ exit: false, sigterm: { exit: true }, context: { exit: exitObserver } }, c => {
+    c.doReject('rejected')
+    assert.equal(metrics.get('exit'), 0)
+
+    c.doThrow('excepted')
+    assert.equal(metrics.get('exit'), 0)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 0)
+
+    c.doSigterm()
     assert.equal(metrics.get('exit'), 1)
   })
 
   metrics.clear()
 
   await runTest({ exit: true, exception: { exit: false }, context: { exit: exitObserver } }, c => {
-    c.doReject('async-fail')
+    c.doReject('rejected')
     assert.equal(metrics.get('exit'), 1)
 
-    c.doThrow('sync-fail')
+    c.doThrow('excepted')
     assert.equal(metrics.get('exit'), 1)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 2)
+
+    c.doSigterm()
+    assert.equal(metrics.get('exit'), 3)
   })
 
   metrics.clear()
 
   await runTest({ exit: true, rejection: { exit: false }, context: { exit: exitObserver } }, c => {
-    c.doReject('async-fail')
+    c.doReject('rejected')
     assert.equal(metrics.get('exit'), 0)
 
-    c.doThrow('sync-fail')
+    c.doThrow('excepted')
     assert.equal(metrics.get('exit'), 1)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 2)
+
+    c.doSigterm()
+    assert.equal(metrics.get('exit'), 3)
+  })
+
+  metrics.clear()
+
+  await runTest({ exit: true, sigint: { exit: false }, context: { exit: exitObserver } }, c => {
+    c.doReject('rejected')
+    assert.equal(metrics.get('exit'), 1)
+
+    c.doThrow('excepted')
+    assert.equal(metrics.get('exit'), 2)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 2)
+
+    c.doSigterm()
+    assert.equal(metrics.get('exit'), 3)
+  })
+
+  metrics.clear()
+
+  await runTest({ exit: true, sigterm: { exit: false }, context: { exit: exitObserver } }, c => {
+    c.doReject('rejected')
+    assert.equal(metrics.get('exit'), 1)
+
+    c.doThrow('excepted')
+    assert.equal(metrics.get('exit'), 2)
+
+    c.doSigint()
+    assert.equal(metrics.get('exit'), 3)
+
+    c.doSigterm()
+    assert.equal(metrics.get('exit'), 3)
   })
 })
 
 tap.test('does nothing if ignore is true for an event type', async assert => {
   await runTest({ exit: true, exception: { ignore: true } }, c => {
-    c.doReject('async-fail')
-    c.doThrow('sync-fail')
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
 
-    assert.equal(c.exitCalls().length, 1)
-    assert.equal(c.logs().length, 1)
-    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'async-fail'])
+    assert.equal(c.exitCalls().length, 3)
+    assert.equal(c.logs().length, 3)
+    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'rejected'])
+    assert.same(c.logs()[1], ['SIGINT Received'])
+    assert.same(c.logs()[2], ['SIGTERM Received'])
   })
 
   await runTest({ exit: true, rejection: { ignore: true } }, c => {
-    c.doReject('async-fail')
-    c.doThrow('sync-fail')
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
 
-    assert.equal(c.exitCalls().length, 1)
-    assert.equal(c.logs().length, 1)
-    assert.same(c.logs()[0], ['Un-caught Exception :', 'sync-fail'])
+    assert.equal(c.exitCalls().length, 3)
+    assert.equal(c.logs().length, 3)
+    assert.same(c.logs()[0], ['Un-caught Exception :', 'excepted'])
+    assert.same(c.logs()[1], ['SIGINT Received'])
+    assert.same(c.logs()[2], ['SIGTERM Received'])
+  })
+
+  await runTest({ exit: true, sigint: { ignore: true } }, c => {
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
+
+    assert.equal(c.exitCalls().length, 3)
+    assert.equal(c.logs().length, 3)
+    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'rejected'])
+    assert.same(c.logs()[1], ['Un-caught Exception :', 'excepted'])
+    assert.same(c.logs()[2], ['SIGTERM Received'])
+  })
+
+  await runTest({ exit: true, sigterm: { ignore: true } }, c => {
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
+
+    assert.equal(c.exitCalls().length, 3)
+    assert.equal(c.logs().length, 3)
+    assert.same(c.logs()[0], ['Un-handled Promise Rejection :', 'rejected'])
+    assert.same(c.logs()[1], ['Un-caught Exception :', 'excepted'])
+    assert.same(c.logs()[2], ['SIGINT Received'])
   })
 })
 
 tap.test('override the logger', async assert => {
   await runTest({ logger: { error: () => {} } }, c => {
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
+
     assert.equals(c.logs().length, 0)
   })
 
   await runTest({ logger: {} }, c => {
+    c.doReject('rejected')
+    c.doThrow('excepted')
+    c.doSigint()
+    c.doSigterm()
+
     assert.equals(c.logs().length, 0)
   })
 })
 
 tap.test('log error in handler', async assert => {
   await runTest({ handler: () => toss('handle-fail') }, c => {
-    c.doThrow('sync-fail')
+    c.doThrow('excepted')
     assert.equals(c.logs().length, 1)
     assert.same(c.logs()[0][0], "Error in handler for 'uncaughtException' event :")
     assert.same(c.logs()[0][1].message, 'handle-fail')
   })
 
   await runTest({ handler: () => toss('handle-fail') }, c => {
-    c.doReject('async-fail')
+    c.doReject('rejected')
     assert.equals(c.logs().length, 1)
     assert.same(c.logs()[0][0], "Error in handler for 'unhandledRejection' event :")
+    assert.same(c.logs()[0][1].message, 'handle-fail')
+  })
+
+  await runTest({ handler: () => toss('handle-fail') }, c => {
+    c.doSigint()
+    assert.equals(c.logs().length, 1)
+    assert.same(c.logs()[0][0], "Error in handler for 'SIGINT' event :")
+    assert.same(c.logs()[0][1].message, 'handle-fail')
+  })
+
+  await runTest({ handler: () => toss('handle-fail') }, c => {
+    c.doSigterm()
+    assert.equals(c.logs().length, 1)
+    assert.same(c.logs()[0][0], "Error in handler for 'SIGTERM' event :")
     assert.same(c.logs()[0][1].message, 'handle-fail')
   })
 })
